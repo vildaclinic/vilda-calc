0a2e003feb138e11cf0a2b13c771bf4c9057ff26
vildaclinic <maciej.flader@gmail.com>
2026-03-12 10:54:25 +0100

    Update service-worker-kalorii.js

--- service-worker-kalorii.js
+++ service-worker-kalorii.js
@@ -18,7 +18,7 @@
  *   bo i tak chcemy zwracać HTML z cache natychmiast.
  */
 
-const SW_VERSION = '0.81.9955';
+const SW_VERSION = '0.81.9999';
 const CACHE_PREFIX = 'pwa-kalorii';
 const SHELL_CACHE = `${CACHE_PREFIX}-shell-v${SW_VERSION}`;
 const RUNTIME_CACHE = `${CACHE_PREFIX}-runtime`;
