16f9fabd827cb950cda6a0d6a0f580b7d4654f7b
vildaclinic <maciej.flader@gmail.com>
2026-06-03 07:32:48 +0200

    Add files via upload

--- service-worker-kalorii.js
+++ service-worker-kalorii.js
@@ -18,7 +18,7 @@
  *   bo i tak chcemy zwracać HTML z cache natychmiast.
  */
 
-const SW_VERSION = '0.9.896';
+const SW_VERSION = '0.9.897';
 const CACHE_PREFIX = 'pwa-kalorii';
 const SHELL_CACHE = `${CACHE_PREFIX}-shell-v${SW_VERSION}`;
 const RUNTIME_CACHE = `${CACHE_PREFIX}-runtime`;
@@ -316,6 +316,7 @@ const CORE_SHELL_URLS = [
   '/vilda_auth_ui.js?v=150',
   '/vilda_auth_ui.js?v=151',
   '/vilda_auth_ui.js?v=152',
+  '/vilda_auth_ui.js?v=153',
   '/vilda_chrome.js?v=8',
   '/vilda_chrome.js?v=11',
   '/vilda_chrome.js?v=12',
@@ -376,6 +377,7 @@ const CORE_SHELL_URLS = [
   '/vilda_auth_ui.css?v=41',
   '/vilda_auth_ui.css?v=42',
   '/vilda_auth_ui.css?v=43',
+  '/vilda_auth_ui.css?v=44',
   '/vilda_file_export.js',
   '/vilda_file_export.js?v=4',
   '/vilda_file_export.js?v=5',
