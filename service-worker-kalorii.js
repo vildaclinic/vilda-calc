056403a5fe5562d1d68c0c751d04bd5a0757379f
vildaclinic <maciej.flader@gmail.com>
2026-06-10 15:08:03 +0200

    Add files via upload

--- service-worker-kalorii.js
+++ service-worker-kalorii.js
@@ -18,7 +18,7 @@
  *   bo i tak chcemy zwracać HTML z cache natychmiast.
  */
 
-const SW_VERSION = '1.0.100';
+const SW_VERSION = '1.0.101';
 const CACHE_PREFIX = 'pwa-kalorii';
 const SHELL_CACHE = `${CACHE_PREFIX}-shell-v${SW_VERSION}`;
 const RUNTIME_CACHE = `${CACHE_PREFIX}-runtime`;
@@ -576,6 +576,7 @@ const CORE_SHELL_URLS = [
   '/vilda_auth_ui.css?v=48',
   '/vilda_auth_ui.css?v=49',
   '/vilda_auth_ui.css?v=50',
+  '/vilda_auth_ui.css?v=51',
   '/vilda_file_export.js',
   '/vilda_file_export.js?v=4',
   '/vilda_file_export.js?v=5',
@@ -884,6 +885,7 @@ const OPTIONAL_ASSETS = [
   '/vilda_shell.js?v=31',
   '/vilda_shell.js?v=32',
   '/vilda_shell.js?v=33',
+  '/vilda_shell.js?v=34',
   '/vilda_shell.css',
   '/vilda_shell.css?v=1',
   '/vilda_shell.css?v=2',
@@ -891,6 +893,7 @@ const OPTIONAL_ASSETS = [
   '/vilda_shell.css?v=4',
   '/vilda_shell.css?v=5',
   '/vilda_shell.css?v=6',
+  '/vilda_shell.css?v=7',
   '/vilda_save_status_indicator.css?v=4',
   '/vilda_frame_sync.js',
   '/vilda_frame_sync.js?v=1'
