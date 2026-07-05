7f4fb5dfa09bc2dc806bed603094474f10da0bce
vildaclinic <maciej.flader@gmail.com>
2026-07-05 18:49:39 +0200

    Add files via upload

--- service-worker-kalorii.js
+++ service-worker-kalorii.js
@@ -18,7 +18,7 @@
  *   bo i tak chcemy zwracać HTML z cache natychmiast.
  */
 
-const SW_VERSION = '1.0.498';
+const SW_VERSION = '1.0.499';
 const CACHE_PREFIX = 'pwa-kalorii';
 const SHELL_CACHE = `${CACHE_PREFIX}-shell-v${SW_VERSION}`;
 const RUNTIME_CACHE = `${CACHE_PREFIX}-runtime`;
@@ -1157,6 +1157,7 @@ const OPTIONAL_ASSETS = [
   '/obesity_therapy_monitor.js?v=7',
   '/obesity_therapy_monitor.js?v=8',
   '/obesity_therapy_monitor.js?v=9',
+  '/obesity_therapy_monitor.js?v=10',
   '/obesity_response_criteria.js',
   '/obesity_response_criteria.js?v=1',
   '/sga_intergrowth_data.js',
